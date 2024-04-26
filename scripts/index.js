1630e9c2a91b10d189f118c959a05f4f34c8c078
Aaron Torres <aaron.torres.dev@gmail.com>
2024-04-26 09:55:20 -0300

    Finished homepage

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,27 @@
-document.getElementsByClassName("brand")[0].addEventListener("click", function (event) {
-    console.log("Hello world");
-});
+// Event listener to listen for clicks on every element that has a class of "brand" (header.html and footer.html brand buttons have it)
+// When clicked, remove the event listener as cleanup, and redirect to "/" which is the home page.
+
+/* TODO: REMOVE THESE, USING ANCHOR INSTEAD */
+/*
+document.querySelectorAll(".brand").forEach((btn) =>
+    btn.addEventListener("click", function (e) {
+        e.target.removeEventListener(e.type, arguments.callee);
+        window.location.href = "/";
+    })
+);
+
+document.querySelectorAll(".footer__email").forEach((btn) =>
+    btn.addEventListener("click", function (e) {
+        e.target.removeEventListener(e.type, arguments.callee);
+        window.location.href = "mailto:bermeo.nicole@gmail.com";
+    })
+);
+
+document.querySelectorAll(".footer__linkedin").forEach((btn) =>
+    btn.addEventListener("click", function (e) {
+        e.target.removeEventListener(e.type, arguments.callee);
+        window.location.href = "https://www.linkedin.com/in/nbermeo/";
+    })
+);
+*/
+/* END TODO */
