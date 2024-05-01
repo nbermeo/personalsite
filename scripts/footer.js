e71105d1379db781b97ef7a477fa258c509950e3
Aaron Torres <aaron.torres.dev@gmail.com>
2024-05-01 17:44:29 -0300

    fix dropdown

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,26 +1,18 @@
-// footer.js
-
-// Create footer element
-var footer = document.createElement("footer");
-footer.innerHTML = `<footer class="footer">
-<!-- Clickable brand button, event listener added by class "brand"-->
-<a class="brand" href="/">
-    <img class="logo" src="./images/logofooter.png" alt="logofooter" />
-    <div class="footer__name">Nicole Bermeo</div>
-</a>
-<div class="footer__contact">
-    <a class="footer__email" href="mailto:bermeo.nicole@gmail.com">
-        <img src="./images/email.svg" alt="email" />
-    </a>
-    <a class="footer__linkedin" href="https://www.linkedin.com/in/nbermeo/" target="_blank">
-        <img src="./images/linkedin.svg" alt="linkedin" />
-    </a>
-</div>
-</footer>
-`;
-
-// Find the body element and append footer
 document.addEventListener("DOMContentLoaded", function () {
-    var body = document.querySelector("body");
-    body.appendChild(footer);
+    var footer = document.querySelector("footer");
+    footer.outerHTML = `<footer class="footer">
+    <!-- Clickable brand button, event listener added by class "brand"-->
+    <a class="brand" href="/">
+        <img class="logo" src="./images/logofooter.png" alt="logofooter" />
+        <div class="footer__name">Nicole Bermeo</div>
+    </a>
+    <div class="footer__contact">
+        <a class="footer__email" href="mailto:bermeo.nicole@gmail.com">
+            <img src="./images/email.svg" alt="email" />
+        </a>
+        <a class="footer__linkedin" href="https://www.linkedin.com/in/nbermeo/" target="_blank">
+            <img src="./images/linkedin.svg" alt="linkedin" />
+        </a>
+    </div>
+</footer> `;
 });
--- scripts/header.js
+++ scripts/header.js
@@ -1,42 +1,41 @@
-// Create header element
-var header = document.createElement("header");
-header.innerHTML = `<!-- Reusable navigation bar that is added to the top of every page on this site via pure JS -->
-<header class="header">
-    <a class="brand" href="/">
-        <img class="logo" src="./images/logo.png" alt="logo" />
-        <div class="header__name">Nicole Bermeo</div>
-    </a>
-    <nav class="header__nav">
-        <!-- from fiddle-->
-        <!-- TODO: Fix this - dropdown disssapears after mouse-down, it should disspaear after mouse-up... if you dont click fast enough on an item in the menu, it will dissapear and the click will not register on the dropdown item-->
-        <!-- TODO: Add on:hover purple version-->
-        <button class="header__works" tabindex="1">
-            <i class="btn2" tabindex="1"></i>
-            <img src="./images/bag.svg" alt="bag" class="header__works-bag" />Works<img src="./images/caretdown.svg" alt="caretdown" />
-            <div class="header__works-dropdown">
-                <a class="header__buildathon dropdown__link" href="./buildathon.html">Buildathon</a>
-                <a class="header__subtrac dropdown__link" href="./subtrac.html">Subtrac</a>
-                <a class="header__prepper dropdown__link" href="./prepper.html">Prepper</a>
-            </div>
-        </button>
-        <!-- 
-        <button class="header__works" onclick="">
-            <img src="./images/bag.svg" alt="bag" />Works<img src="./images/caretdown.svg" alt="caretdown" />
-            <ul class="header__works-dropdown">
-                <li><a class="header__buildathon dropdown__link" href="./buildathon.html">Buildathon</a></li>
-                <li><a class="header__subtrac dropdown__link" href="./subtrac.html">Subtrac</a></li>
-                <li><a class="header__prepper dropdown__link" href="./prepper.html">Prepper</a></li>
-            </ul>
-        </button>
-    -->
-        <!-- TODO: Add purple on:hover version-->
-        <a class="header__about" href="./about.html"><img src="./images/usercircle.svg" alt="usercircle" />About</a>
-        <button class="header__contact" onclick=""><img src="./images/envelope.svg" alt="envelope" />Contact<img src="./images/caretdownpurple.svg" alt="caretdownpurple" /></button>
-        <a class="button__purple" href="./documents/resume.pdf" target="_blank"><img src="./images/scroll.svg" alt="scroll" />View Resume</a>
-    </nav>
-</header>`;
-
 document.addEventListener("DOMContentLoaded", function () {
-    var body = document.querySelector("body");
-    body.insertBefore(header, body.firstChild);
+    // Select the existing header element, and add in the header content
+    var header = document.querySelector("header");
+
+    header.outerHTML = `<!-- Reusable navigation bar that is added to the top of every page on this site via pure JS -->
+        <header class="header">
+            <a class="brand" href="/">
+                <img class="logo" src="./images/logo.png" alt="logo" />
+                <div class="header__name">Nicole Bermeo</div>
+            </a>
+            <nav class="header__nav">
+                <button id="menu-toggle" class="dropbtn"><img src="./images/bag.svg" alt="bag" />Works<img src="./images/caretdown.svg" alt="caretdown" />
+                </button>
+                <div id="dropdown-content" class="dropdown-content">
+                    <a class="header__buildathon dropdown__link" href="./buildathon.html">Buildathon</a>
+                    <a class="header__subtrac dropdown__link" href="./subtrac.html">Subtrac</a>
+                    <a class="header__prepper dropdown__link" href="./prepper.html">Prepper</a>
+                </div>
+
+                <!-- TODO: Add purple on:hover version-->
+                <a class="header__about" href="./about.html"><img src="./images/usercircle.svg" alt="usercircle" />About</a>
+                <button class="header__contact" onclick=""><img src="./images/envelope.svg" alt="envelope" />Contact<img src="./images/caretdownpurple.svg" alt="caretdownpurple" /></button>
+                <a class="button__purple" href="./documents/resume.pdf" target="_blank"><img src="./images/scroll.svg" alt="scroll" />View Resume</a>
+            </nav>
+        </header>`;
+
+    // on click listeners to handle "Works" and "Contact" dropdown menu clicks
+    var menuToggle = document.getElementsByClassName("dropbtn")[0];
+    var dropdownContent = document.getElementsByClassName("dropdown-content")[0];
+
+    menuToggle.addEventListener("click", function (event) {
+        dropdownContent.style.display = dropdownContent.style.display === "block" ? "none" : "block";
+        event.stopPropagation();
+    });
+
+    document.addEventListener("click", function (event) {
+        if (!dropdownContent.contains(event.target) && dropdownContent.style.display === "block") {
+            dropdownContent.style.display = "none";
+        }
+    });
 });
