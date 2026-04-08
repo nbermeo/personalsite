54a7559fe63a521143e06bb0846cfbb361c6c42f
Aaron Torres <aaron.torres.dev@gmail.com>
2026-04-08 23:14:07 +0000

    lazy loading

--- scripts/footer.js
+++ scripts/footer.js
@@ -2,15 +2,15 @@ document.addEventListener("DOMContentLoaded", function () {
     var footer = document.querySelector("footer");
     footer.outerHTML = `<footer class="footer">
     <a class="brand" href="/">
-        <img class="logo" src="./images/logofooter.svg" alt="logofooter" />
+        <img class="logo" src="./images/logofooter.svg" width="68" height="68" loading="lazy" alt="logofooter" />
         <div class="footer__name">Nicole Bermeo</div>
     </a>
     <div class="footer__contact">
         <a class="footer__email" href="mailto:bermeo.nicole@gmail.com">
-            <img src="./images/email.svg" width="32" height="32" alt="email" />
+            <img src="./images/email.svg" width="32" height="32" loading="lazy" alt="email" />
         </a>
         <a class="footer__linkedin" href="https://www.linkedin.com/in/nbermeo/" target="_blank">
-            <img src="./images/linkedin.svg" width="32" height="32" alt="linkedin" />
+            <img src="./images/linkedin.svg" width="32" height="32" loading="lazy" alt="linkedin" />
         </a>
     </div>
 </footer> `;
